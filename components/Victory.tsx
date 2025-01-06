9c5203551361b264f0ef6d4d59146f60110455f8
PG-Sachin-Dangi <Sachin@propertyguru.tech>
2025-01-06 10:55:28 +0530

    feat: initial setup

---
+++ components/Victory.tsx
@@ -0,0 +1,11 @@
+import { ThemedText } from './ThemedText';
+import { ThemedView } from './ThemedView';
+
+export const Victory: React.FC = () => {
+  return (
+    <ThemedView>
+      <ThemedText>Victory Graph POC</ThemedText>
+      {/* code */}
+    </ThemedView>
+  );
+};
