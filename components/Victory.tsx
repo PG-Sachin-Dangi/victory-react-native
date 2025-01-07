f482600604fecc789a54b834904b5f972d93a72f
PG-Sachin-Dangi <Sachin@propertyguru.tech>
2025-01-07 11:13:01 +0530

    feat: linear gradient bar chart

--- components/Victory.tsx
+++ components/Victory.tsx
@@ -9,7 +9,12 @@ import {
   Line,
   useChartPressState,
 } from 'victory-native';
-import { Circle, useFont } from '@shopify/react-native-skia';
+import {
+  Circle,
+  LinearGradient,
+  useFont,
+  vec,
+} from '@shopify/react-native-skia';
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
@@ -25,79 +30,107 @@ export const ListingPerformanceGraph = () => {
 };
 
 export const LineChart = () => {
-  const data = Array.from({ length: 11 }, (_, i) => ({
-    x: i,
-    y: 40 + 30 * Math.random(),
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const lineChartData = Array.from({ length: 12 }, (_, index) => ({
+    x: months[index],
+    y: 100 * Math.random(),
   }));
 
-  const barGraphData = Array.from({ length: 6 }, (_, index) => ({
-    // Starting at 1 for Jaunary
-    month: index + 1,
-    // Randomizing the listen count between 100 and 50
-    listenCount: Math.floor(Math.random() * (100 - 50 + 1)) + 50,
+  const barChartData = Array.from({ length: 12 }, (_, index) => ({
+    x: months[index],
+    // any 4 values 100 others 0 at random
+    y: Math.random() > 0.75 ? 100 : 0,
   }));
 
-  const { state, isActive } = useChartPressState({ x: 0, y: { y: 0 } });
-  const font = useFont(require('../assets/fonts/SpaceMono-Regular.ttf'), 16);
-
-  const barGraphPoints = barGraphData.map((bar, index) => ({
-    x: index * bar.listenCount,
-    xValue: index * bar.listenCount,
-    y: 0,
-    yValue: 0,
-  }));
+  const { state, isActive } = useChartPressState({ x: 'Jan', y: { y: 0 } });
+  const font = useFont(require('../assets/fonts/SpaceMono-Regular.ttf'), 8);
 
   return (
-    <CartesianChart
-      chartPressState={state}
-      data={data}
-      xKey="x"
-      yKeys={['y']}
-      xAxis={{
-        font: font,
-        tickCount: 10,
-      }}
-      yAxis={[{ font: font, tickCount: 5 }]}
-      domainPadding={{ left: 50, right: 50, top: 30 }}
-      padding={16}
-    >
-      {({ points, chartBounds }) => (
-        //👇 pass a PointsArray to the Line component, y0, as well as options.
-        <>
-          <Line
-            points={points.y}
-            curveType="linear"
-            color="#0071EA"
-            strokeWidth={3}
-            animate={{ type: 'timing', duration: 300 }}
-          />
-          <Area
-            points={points.y}
-            y0={chartBounds.bottom}
-            curveType="linear"
-            color="#0071EA4D"
-            animate={{ type: 'timing', duration: 300 }}
-          />
+    <>
+      <View style={styles.lineChart}>
+        <CartesianChart
+          chartPressState={state}
+          data={lineChartData}
+          xKey="x"
+          yKeys={['y']}
+          xAxis={{
+            font: font,
+            tickCount: 12,
+          }}
+          domain={{ x: [1, 11], y: [0, 100] }}
+          yAxis={[{ font: font }]}
+          domainPadding={{ left: 50, right: 50, top: 30 }}
+        >
+          {({ points, chartBounds }) => (
+            //👇 pass a PointsArray to the Line component, y0, as well as options.
+            <>
+              <Line
+                points={points.y}
+                curveType="linear"
+                color="#0071EA"
+                strokeWidth={3}
+                animate={{ type: 'timing', duration: 300 }}
+              />
+              <Area
+                points={points.y}
+                y0={chartBounds.bottom}
+                curveType="linear"
+                color="#0071EA4D"
+                animate={{ type: 'timing', duration: 300 }}
+              />
 
-          {isActive && <ToolTip x={state.x.position} y={state.y.y.position} />}
+              {isActive && (
+                <ToolTip x={state.x.position} y={state.y.y.position} />
+              )}
+            </>
+          )}
+        </CartesianChart>
+      </View>
 
-          {barGraphData.map((bar, index) => (
+      <View style={styles.barChart}>
+        <CartesianChart
+          data={barChartData}
+          xKey="x"
+          yKeys={['y']}
+          xAxis={{
+            font: font,
+            tickCount: 12,
+          }}
+          yAxis={[{ font: font }]}
+          domain={{ x: [1, 11], y: [0, 100] }}
+          domainPadding={{ left: 50, right: 50, top: 30 }}
+        >
+          {({ points, chartBounds }) => (
             <Bar
-              //   opacity={0.5}
-              color="#F2A92666"
-              key={index}
-              chartBounds={{
-                top: 0,
-                left: 0,
-                right: bar.listenCount, // width of the bar
-                bottom: bar.listenCount,
-              }}
-              points={barGraphPoints}
-            />
-          ))}
-        </>
-      )}
-    </CartesianChart>
+              points={points.y}
+              chartBounds={chartBounds}
+              barCount={barChartData.length}
+              animate={{ type: 'timing', duration: 300 }}
+            >
+              <LinearGradient
+                start={vec(0, 0)}
+                end={vec(0, 400)}
+                colors={['#F2A92666', '#FFFFFF66']}
+              />
+            </Bar>
+          )}
+        </CartesianChart>
+      </View>
+    </>
   );
 };
 
@@ -117,5 +150,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     gap: 16,
+  },
+  lineChart: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 1,
+  },
+  barChart: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 0,
   },
 });
